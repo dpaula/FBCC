2fd63ec55d82a08d0ef4fe513766e92ff0d18a37
fernando de paula <fernando.dpaula@gmail.com>
2013-05-26 23:43:26 -0300

    primeira classe

---
+++ dp-inc/src/arquivos/ClasseScan.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package arquivos;
+
+
+/**
+ * @author Fernando
+ *
+ */
+public class ClasseScan {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
